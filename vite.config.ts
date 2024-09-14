683951f043b5c17108c511ed7586e2bce52844f6
liuyong <liuyong@yunzhanxinxi.cn>
2024-09-14 17:42:08 +0800

    update: yml

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/web-vue-app/',
+  base: '/web-app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
