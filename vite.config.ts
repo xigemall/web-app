5854823cce7fed48599572124cf96aef5e8922ef
liuyong <liuyong@yunzhanxinxi.cn>
2024-09-14 17:32:04 +0800

    update: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/web-vue-app/'
+  plugins: [
+    vue(),
+  ],
+  base: '/web-vue-app/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
